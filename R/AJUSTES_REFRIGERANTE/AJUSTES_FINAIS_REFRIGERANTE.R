870e87f155cff737ac01e82e6f9ebd95955f277a
Emir Smaka <emir.smaka@gmail.com>
2021-02-17 08:23:29 -0400

    Versao final do script de ajustes finais para tabela de refrigerantes.

--- R/AJUSTES_REFRIGERANTE/AJUSTES_FINAIS_REFRIGERANTE.R
+++ R/AJUSTES_REFRIGERANTE/AJUSTES_FINAIS_REFRIGERANTE.R
@@ -20,6 +20,7 @@ df_refrigerante <- rename(df_refrigerante,PROD_FATOR_MULTIPLICADOR = 'FATOR_MULT
 df_refrigerante <- rename(df_refrigerante,PROD_USEFAZ_AJUSTADO = 'UNIDADE_SEFAZ')
 df_refrigerante <- rename(df_refrigerante,PROD_QSEFAZ_AJUSTADO = 'QTE_SEFAZ')
 df_refrigerante <- rename(df_refrigerante,PROD_VUNSEFAZ_AJUSTADO = 'VLR_UNITARIO_SEFAZ')
+df_refrigerante[,"PROD_EMBALAGEM_AJUSTADO"] <- as.character()
 ######################################################################################
 
 #### CONVERTER UNIDADE MEDIDA DE L PARA ML BEM COMO O VOLUME
@@ -46,3 +47,26 @@ df_refri_ks <- df_refri_ml[i_ks,]
 df_refri_lata <- df_refri_ml[i_lata,]
 df_refri_ml <- anti_join(df_refri_ml,df_refri_ks,by=c("IDNFE","DET_NITEM"))
 df_refri_ml <- anti_join(df_refri_ml,df_refri_lata,by=c("IDNFE","DET_NITEM"))
+
+df_refri_ks$PROD_EMBALAGEM_AJUSTADO <- "GARRAFA"
+df_refri_lata$PROD_EMBALAGEM_AJUSTADO <- "LATA"
+df_refri_pet$PROD_EMBALAGEM_AJUSTADO <- "PET"
+df_refri_ml$PROD_EMBALAGEM_AJUSTADO <- "SEM AJUSTE"
+df_refrigerante$PROD_EMBALAGEM_AJUSTADO <- "SEM AJUSTE"
+
+df_refrigerante <- rbind(df_refri_ks,df_refri_lata,df_refri_ml,df_refri_pet,df_refrigerante)
+rm(df_refri_ks,df_refri_lata,df_refri_ml,df_refri_pet,i_ks,i_lata)
+gc(reset = T)
+
+tb_refrigerante <- df_refrigerante%>%
+  select(IDNFE,DET_NITEM,PROD_CPROD_SEFAZ_DETALHADO,PROD_XPROD_SEFAZ_DETALHADO,PROD_QSEFAZ_AJUSTADO
+         ,PROD_USEFAZ_AJUSTADO,PROD_VUNSEFAZ_AJUSTADO,PROD_FATOR_MULTIPLICADOR,PROD_VOLUME_SEFAZ_AJUSTADO
+         ,PROD_UNIDADE_MEDIDA_SEFAZ_AJUSTADO,PROD_EMBALAGEM_AJUSTADO)
+
+fwrite(tb_refrigerante,"./DADOS/TB_REFRIGERANTE.csv",sep = ";")
+
+rm(df_refrigerante,tb_refrigerante)
+gc(reset = T)
+
+######################## FIM AJUSTES FINAIS REFRIGERANTES ########################
+
